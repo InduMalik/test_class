373fcaf73aad177ec2f0a8b55d6d5aa17fc03256
InduMalik <indumalik92@gmail.com>
2024-09-06 13:07:38 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,57 @@
+
+
+    function d() {
+        let b=document.getElementById("r1").value;
+
+        if(b==1)
+        {
+        var img = document.createElement("img");
+        img.src = "4-dots.png";
+        var src = document.getElementById("div");
+        src.appendChild(img);
+       
+        }
+        else{
+            if(b==2)
+                {
+                var img = document.createElement("img");
+                img.src = "4-dots.png";
+                var src = document.getElementById("div");
+                src.appendChild(img);
+
+                var img1 = document.createElement("img");
+                img1.src = "4-dots.png";
+                var src = document.getElementById("div");
+                src.appendChild(img1);
+               
+                }
+                else {
+                    if(b==3)
+                        {
+                        var img = document.createElement("img");
+                        img.src = "4-dots.png";
+                        var src = document.getElementById("div");
+                        src.appendChild(img);
+        
+                        var img1 = document.createElement("img");
+                        img1.src = "4-dots.png";
+                        var src = document.getElementById("div");
+                        src.appendChild(img1);
+        
+                        var img2 = document.createElement("img");
+                        img2.src = "4-dots.png";
+                        var src = document.getElementById("div");
+                        src.appendChild(img2);
+                       
+                        }
+                    }
+                
+        }
+        
+        
+    }
+      
+
+
+
+
---
+++ b.js
@@ -0,0 +1,157 @@
+
+
+function d() {
+    let b=document.getElementById("r1").value;
+
+    switch(b)
+    {
+    case 1: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img);
+    case 2: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img);
+    var img2 = document.createElement("img");
+    img2.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img2);
+    case 3: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img);
+    var img2 = document.createElement("img");
+    img2.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img2);
+    var img3 = document.createElement("img");
+    img3.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img3);
+    case 4: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img);
+    var img2 = document.createElement("img");
+    img2.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img2);
+    var img3 = document.createElement("img");
+    img3.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img3);
+    var img4 = document.createElement("img");
+    img4.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img4);
+    var img5 = document.createElement("img");
+    img5.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img5);
+    case 5: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img);
+    var img2 = document.createElement("img");
+    img2.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img2);
+    var img3 = document.createElement("img");
+    img3.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img3);
+    var img4 = document.createElement("img");
+    img4.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img4);
+    var img5 = document.createElement("img");
+    img5.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img5);
+    case 6: var img = document.createElement("img");
+    img.src = "4-dots.png";
+    var img2 = document.createElement("img");
+    var img2 = document.createElement("img");
+    img2.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img2);
+    var img3 = document.createElement("img");
+    img3.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img3);
+    var img4 = document.createElement("img");
+    img4.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img4);
+    var img5 = document.createElement("img");
+    img5.src = "4-dots.png";
+    var src = document.getElementById("div");
+    src.appendChild(img5);
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     if(b==1)
+//     {
+//     var img = document.createElement("img");
+//     img.src = "4-dots.png";
+//     var src = document.getElementById("div");
+//     src.appendChild(img);
+   
+//     }
+//     else{
+//         if(b==2)
+//             {
+//             var img = document.createElement("img");
+//             img.src = "4-dots.png";
+//             var src = document.getElementById("div");
+//             src.appendChild(img);
+
+//             var img1 = document.createElement("img");
+//             img1.src = "4-dots.png";
+//             var src = document.getElementById("div");
+//             src.appendChild(img1);
+           
+//             }
+//             else {
+//                 if(b==3)
+//                     {
+//                     var img = document.createElement("img");
+//                     img.src = "4-dots.png";
+//                     var src = document.getElementById("div");
+//                     src.appendChild(img);
+    
+//                     var img1 = document.createElement("img");
+//                     img1.src = "4-dots.png";
+//                     var src = document.getElementById("div");
+//                     src.appendChild(img1);
+    
+//                     var img2 = document.createElement("img");
+//                     img2.src = "4-dots.png";
+//                     var src = document.getElementById("div");
+//                     src.appendChild(img2);
+                   
+//                     }
+//                 }
+            
+//     }
+    
+    
+// }
+  
+
+
+
+
